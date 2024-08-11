6d2351bd1638942bb9c358cb21024b902d0d0182
Ayush69420504 <rijo.slg@gmail.com>
2024-08-11 16:00:06 +0530

    Practise learning of jquery.terminal done real work begins now

---
+++ index.js
@@ -0,0 +1,83 @@
+const user = 'ayush'
+const server = 'doge69420504'
+
+const formatter = new Intl.ListFormat('en', {
+    style: 'long',
+    type: 'conjunction',
+  });  
+
+const root = '~';
+let cwd = root;
+
+const commands = {
+    help() {
+        term.echo(`List of available commands: ${help}`);
+    },
+    echo(...args) {
+        term.echo(args.join(' '));
+    },
+	cd(dir = null) {
+        if (dir === null || (dir === '..' && cwd !== root)) {
+            cwd = root;
+        } else if (dir.startsWith('~/') && dirs.includes(dir.substring(2))) {
+            cwd = dir;
+        } else if (dirs.includes(dir)) {
+            cwd = root + '/' + dir;
+        } else {
+            this.error('Wrong directory');
+        }
+    }
+};
+
+const command_list = ['clear', 'exit'].concat(Object.keys(commands));
+const help = formatter.format(command_list);
+
+const font = 'ANSI Shadow';
+
+figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts/' });
+figlet.preloadFonts([font], ready);
+
+function prompt() {
+    return `[[;green;]${user}@${server}][[;blue;]${cwd}$]`;
+}
+
+const term = $('body').terminal(commands, {
+    greetings: false,
+    checkArity: false,
+    completion: true,
+    prompt
+});
+
+term.pause();
+
+function ready() {
+    const seed = rand(256);
+    term.echo(() => rainbow(render('This is test'), seed), {ansi: true});
+    term.echo('[[;#fff;]Another test]').resume();
+}
+
+function rand(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function render(text) {
+    const cols = term.cols();
+    return figlet.textSync(text, {
+        font: font,
+        width: cols,
+        whitespaceBreak: true
+    });
+}
+
+function rainbow(string, seed) {
+    return lolcat.rainbow(function(char, color) {
+        char = $.terminal.escape_brackets(char);
+        return `[[;${hex(color)};]${char}]`;
+    }, string, seed).join('\n');
+}
+
+function hex(color) {
+    return '#' + [color.red, color.green, color.blue].map(n => {
+        return n.toString(16).padStart(2, '0');
+    }).join('');
+}
